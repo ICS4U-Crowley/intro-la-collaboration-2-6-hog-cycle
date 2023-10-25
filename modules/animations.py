a003d5b5dbfc087d5774e6d2e87ffdde97c7d7df
danielgebara <128635331+danielgebara@users.noreply.github.com>
2023-10-25 13:24:29 -0400

    complete animation

--- modules/animations.py
+++ modules/animations.py
@@ -3,12 +3,14 @@ from turtle import *
 def animate():
     print("Action d'animation")
     try:
-        # TODO code d'animation turtle ici
+        t = Turtle()
+        t.speed(5)  
+        for _ in range(4):
+            t.forward(100) 
+            t.left(90)  
         mainloop()
     except Terminator as e:
         print('...la fenêtre a été fermée')
 
-# Code pour tester cette module indépendamment du programme principal
 if __name__ == "__main__":  
-    # TODO ajouter des tests unitaires pour les fonctions
     animate()
