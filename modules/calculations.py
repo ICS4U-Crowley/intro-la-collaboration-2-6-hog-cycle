d0b43caa86b44b180599079f956b4bb75ea4f20b
Ambussin <128636077+Ambussin@users.noreply.github.com>
2023-10-26 10:11:47 -0400

    Nouvelle code

--- modules/calculations.py
+++ modules/calculations.py
@@ -1,60 +1,40 @@
-"""
-Définition des fonctions pour les calculs de la 
-calculatrice
-"""
-# Addition
-def add(x, y):
+# Fonction d'addition
+def addition():
+    x = float(input("Entrez le premier nombre : "))
+    y = float(input("Entrez le deuxième nombre : "))
     return x + y
 
-# Soustraction
-def subtract(x, y):
+# Fonction de soustraction
+def soustraction():
+    x = float(input("Entrez le premier nombre : "))
+    y = float(input("Entrez le deuxième nombre : "))
     return x - y
 
-# Multiplication
-def multiply(x, y):
+# Fonction de multiplication
+def multiplication():
+    x = float(input("Entrez le premier nombre : "))
+    y = float(input("Entrez le deuxième nombre : "))
     return x * y
 
-# Division
-def divide(x, y):
+# Fonction de division
+def division():
+    x = float(input("Entrez le numérateur : "))
+    y = float(input("Entrez le dénominateur : "))
     if y == 0:
         return "Division par zéro"
     return x / y
 
-# Calculatrice
-def calc():
-    while True:
-        print("Calculatrice:")
-        print("1. Addition")
-        print("2. Soustraction")
-        print("3. Multiplication")
-        print("4. Division")
-        print("5. Quitter")
+# Code pour tester ce module indépendamment du programme principal
+if __name__ == "__main__":
+    # Tests unitaires pour les fonctions
+    resultat = addition()
+    print(f"Addition : Résultat = {resultat}")
 
-        choice = input("Sélectionnez une option: ")
+    resultat = soustraction()
+    print(f"Soustraction : Résultat = {resultat}")
 
-        if choice == '5':
-            break
+    resultat = multiplication()
+    print(f"Multiplication : Résultat = {resultat}")
 
-        if choice in ['1', '2', '3', '4']:
-            x = float(input("Entrez le premier nombre: "))
-            y = float(input("Entrez le deuxième nombre: "))
-
-            if choice == '1':
-                result = add(x, y)
-            elif choice == '2':
-                result = subtract(x, y)
-            elif choice == '3':
-                result = multiply(x, y)
-            elif choice == '4':
-                result = divide(x, y)
-
-            print("Résultat :", result)
-        else:
-            print("Option invalide")
-
-calc()
-
-# Code pour tester cette module indépendamment du programme principal
-if __name__ == "__main__":  
-    # TODO ajouter des tests pour les fonctions
-    calc()
+    resultat = division()
+    print(f"Division : Résultat = {resultat}")
