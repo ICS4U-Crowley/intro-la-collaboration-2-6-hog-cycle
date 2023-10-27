4eec0f3153d5e531f553b3dc5a1d519cb516a73e
Ambussin <128636077+Ambussin@users.noreply.github.com>
2023-10-27 13:24:45 -0400

    pretty close... just a few more tests

--- modules/calculations.py
+++ modules/calculations.py
@@ -1,40 +1,63 @@
+import cli as cli
+
+# Définition des fonctions pour les calculs de la calculatrice
+
 # Fonction d'addition
-def addition():
-    x = float(input("Entrez le premier nombre : "))
-    y = float(input("Entrez le deuxième nombre : "))
+
+def add(x, y):
     return x + y
 
 # Fonction de soustraction
-def soustraction():
-    x = float(input("Entrez le premier nombre : "))
-    y = float(input("Entrez le deuxième nombre : "))
+def subtract(x, y):
     return x - y
 
 # Fonction de multiplication
-def multiplication():
-    x = float(input("Entrez le premier nombre : "))
-    y = float(input("Entrez le deuxième nombre : "))
+def multiply(x, y):
     return x * y
 
 # Fonction de division
-def division():
-    x = float(input("Entrez le numérateur : "))
-    y = float(input("Entrez le dénominateur : "))
+def divide(x, y):
     if y == 0:
         return "Division par zéro"
     return x / y
 
 # Code pour tester ce module indépendamment du programme principal
-if __name__ == "__main__":
-    # Tests unitaires pour les fonctions
-    resultat = addition()
-    print(f"Addition : Résultat = {resultat}")
+def calc():
+    while True:
+        item = ["Addition", "Soustraction", "Multiplication", "Division"]
+        choice = cli.menu(item)
+        
+        if choice not in ['1', '2', '3', '4']:
+            print("Choix invalide. Veuillez choisir une option valide.")
+            continue
+        
+        x = float(input("Entrez le premier nombre : "))
+        y = float(input("Entrez le deuxième nombre : "))
 
-    resultat = soustraction()
-    print(f"Soustraction : Résultat = {resultat}")
+        if choice == '1':
+            result = add(x, y)
+            print(f"Résultat : {x} + {y} = {result}")
+        elif choice == '2':
+            result = subtract(x, y)
+            print(f"Résultat : {x} - {y} = {result}")
+        elif choice == '3':
+            result = multiply(x, y)
+            print(f"Résultat : {x} * {y} = {result}")
+        elif choice == '4':
+            result = divide(x, y)
+            print(f"Résultat : {x} / {y} = {result}")
 
-    resultat = multiplication()
-    print(f"Multiplication : Résultat = {resultat}")
+        another_calculation = input("Voulez-vous effectuer une autre opération ? (O/N) : ").strip().lower()
+        if another_calculation != 'o':
+            break
 
-    resultat = division()
-    print(f"Division : Résultat = {resultat}")
+# Fonction de test pour les opérations de calcul
+def test_calculator_operations():
+    try:
+        assert add(5, 3) == 9
+        assert subtract(10, 4) == 6
+        assert multiply(7, 2) == 14
+        assert divide(8, 2) == 4
+        assert divide(6, 0) == "Division par zéro"
+    except AssertionError :
+        print("failed")
