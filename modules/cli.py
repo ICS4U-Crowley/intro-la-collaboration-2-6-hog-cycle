7de1010c45e31fe95421c8599fab1cc3535d156c
Thunderking619 <128635676+Thunderking619@users.noreply.github.com>
2023-10-27 13:18:48 -0400

    Fin

--- modules/cli.py
+++ modules/cli.py
@@ -7,7 +7,7 @@ l'interface utilisateur à la console
 
 def greet():
     """() -> None"""
-    
+    # just un petit message au debut du programme. 
     print("Bien veneu a ce programme tu peut reagarder un animation dun cube puis tu peut faire des calcul")
 
 def menu(items):
@@ -15,10 +15,11 @@ def menu(items):
     Retourne le nombre de la position de l'item dans la liste des items.
     Le 1e item retourne la valeur 1, etc. Aucune validation des valeurs invalides.
     """
-    
+    # sa print chaque element dans un liste puis cest numero en ordre, puis demande au lutilisateur de saisir un choix
+    # dans la list. 
     for i in items:
         print(items.index(i)+1, i)
-    return input ("saisir votre choix (metter un des trois numero): ")
+    return input ("saisir votre choix (doit etre un des numero): ")
 
 def clear():
     """() -> None"""
@@ -26,7 +27,7 @@ def clear():
 
 def close():
     """() -> None"""
-   
+   # message a la fin de la programme. 
     print("Merci pour utiliser notre programme a la prochain.")
 
 # Code pour tester cette module indépendamment du programme principal
@@ -37,4 +38,4 @@ if __name__ == "__main__":
     choix = menu(["option 1", "option 2", "option 3"])
     print("choix =", choix)
     close()
-    menuCalc()
+    
